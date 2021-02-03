dceff8447683faf68101aa020d4aeae1992bcf22
Fitz-commits <cdelaby@student.s19.be>
2021-02-03 18:58:58 +0100

    final version

--- class_builder.py
+++ class_builder.py
@@ -7,20 +7,14 @@ def main():
 		try :
 			x = input('Enter class name : ')
 			lst = x.split(" ")
-			print(lst)
 		except KeyboardInterrupt:
 			exit()
-			for i in lst :
-				try :
-					float(i)
-					print("is a number")
-					continue
-				except ValueError:
-					if (i == "exit"):
-						exit()
-					test = MyClass(i)
-					test.create_cpp()
-					test.create_hpp()
+		for i in lst :
+			if (i == "exit"):
+				exit()
+			test = MyClass(i)
+			test.create_cpp()
+			test.create_hpp()
 
 if __name__ == "__main__":
 	main()
